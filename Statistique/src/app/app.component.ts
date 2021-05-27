8f575d4e565e271d6ddf9d69715d4232c153ccfd
ybourgin <yvesbourgin42@gmail.com>
2021-05-27 12:19:45 +0200

    exercice 2

--- Statistique/src/app/app.component.ts
+++ Statistique/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Statistique } from './models/statistique';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Statistique';
+  tabStatistiques: Statistique[] = [
+    { _id: '1', titre: 'population1', valeur: 200000, appreciation: 'blabla' },
+    { _id: '2', titre: 'population2', valeur: 12000, appreciation: 'blabla' },
+  ];
 }
--- Statistique/src/app/models/statistique.ts
+++ Statistique/src/app/models/statistique.ts
@@ -1,2 +1,8 @@
 export class Statistique {
+  constructor(
+    public _id: string,
+    public titre: string,
+    public valeur: number,
+    public appreciation: string
+  ) {}
 }
