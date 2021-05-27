6558df5046a9e7072a1d937572c9095b8c7f11cb
ybourgin <yvesbourgin42@gmail.com>
2021-05-27 15:29:23 +0200

    Exercice4

--- Statistique/src/app/app.component.ts
+++ Statistique/src/app/app.component.ts
@@ -8,5 +8,19 @@ import { Statistique } from './models/statistique';
 })
 export class AppComponent {
   public statistique1 = new Statistique('1', 'population1', 200000, 'blabla');
-  public statistique2 = new Statistique('2', 'population2', 12000, 'blabla');
+  public statistique2 = new Statistique('2', 'population2', 12000, 'blablabla');
+  public statistique3 = new Statistique(
+    '3',
+    'population3',
+    56000,
+    'blablablabla'
+  );
+  public tabStatistique: Statistique[] = [];
+
+  constructor() {
+    this.tabStatistique.push(this.statistique1, this.statistique2);
+    setTimeout(() => {
+      this.tabStatistique.push(this.statistique3);
+    }, 1000);
+  }
 }
